0dc2fd3e20207db8292c9683b2744c01562711b1
xkong-anaconda <xkong@anaconda.com>
2026-06-12 10:43:37 -0400

    Update wandb to 0.27.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,19 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-pushd gpu_stats
-cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
+# Bundle Rust licenses for xpu (formerly gpu_stats) and parquet-rust-wrapper
+pushd xpu
+cargo-bundle-licenses --format yaml --output ../THIRDPARTY_XPU.yml
 popd
+
+pushd parquet-rust-wrapper
+cargo-bundle-licenses --format yaml --output ../THIRDPARTY_PARQUET.yml
+popd
+
+# Unset CARGO_BUILD_TARGET: conda's rust compiler activation sets it, but
+# xpu/hatch.py hardcodes "target/release/" and doesn't account for the
+# target-triple subdirectory that CARGO_BUILD_TARGET introduces.
+unset CARGO_BUILD_TARGET
 
 ${PYTHON} -m pip install --no-deps --no-build-isolation -vv .
 
